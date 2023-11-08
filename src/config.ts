45e110270766cec264d8c9cdbd191891e9f1b881
Loxess <loxes345@gmail.com>
2023-11-08 18:37:58 -0500

    removetokenxd

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
-//OTI4NDU4MDU1MjI0NTM3MTI4.GrgakP.aQhHLmbF-1En8zxrYX_caJHkVWXxXiW5ys0Q4w
+
 export const TOKEN = process.env.TOKEN || '';
 export const APPLICATION_ID = process.env.APPLICATION_ID || '';
 export const GUILD_ID = process.env.GUILD_ID || '';
