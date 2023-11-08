1bfecddc9b9599524a7772f57d92da24948ef7bb
Loxess <loxes345@gmail.com>
2023-11-08 18:36:18 -0500

    allow intents

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,8 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
-
-const token1 = 'OTI4NDU4MDU1MjI0NTM3MTI4';
-const token2 = 'GrgakP';
-const token3 = 'aQhHLmbF';
-const token4 = '1En8zxrYX';
-const token5 = 'caJHkVWXxX';
-const token6 = 'iW5ys0Q4w';
-export const TOKEN =
-  token1 + '.' + token2 + '.' + token3 + '-' + token4 + '_' + token5 + token6;
+//OTI4NDU4MDU1MjI0NTM3MTI4.GrgakP.aQhHLmbF-1En8zxrYX_caJHkVWXxXiW5ys0Q4w
+export const TOKEN = process.env.TOKEN || '';
 export const APPLICATION_ID = process.env.APPLICATION_ID || '';
-export const GUILD_ID = '1125170418853097542';
-export const EXCHANGE_API = 'f853e108c992d5420374b9a0b77490d1';
+export const GUILD_ID = process.env.GUILD_ID || '';
+export const EXCHANGE_API = process.env.EXCHANGE_API || '';
