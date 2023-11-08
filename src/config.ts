f8000de25decd1b6b085fba8df7da6c7f8245251
Loxess <loxes345@gmail.com>
2023-11-08 18:26:55 -0500

    cambio de token

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const token1 = 'OTI4NDU4MDU1MjI0NTM3MTI4';
+const token2 = 'GrgakP';
+const token3 = 'aQhHLmbF';
+const token4 = '1En8zxrYX';
+const token5 = 'caJHkVWXxX';
+const token6 = 'iW5ys0Q4w';
 export const TOKEN =
-  'OTI4NDU4MDU1MjI0NTM3MTI4.YdZD8g.xPUWMGHrB-ztoSI6LeP2dnW7lLY';
+  token1 + '.' + token2 + '.' + token3 + '-' + token4 + '_' + token5 + token6;
 export const APPLICATION_ID = process.env.APPLICATION_ID || '';
 export const GUILD_ID = '1125170418853097542';
 export const EXCHANGE_API = 'f853e108c992d5420374b9a0b77490d1';
