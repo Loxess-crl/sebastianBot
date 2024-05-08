7d160c55039b809700b35c5b7f2bf4646170eb2c
Loxess <loxes345@gmail.com>
2024-05-07 21:37:50 -0500

    empty message fixed

--- src/listeners/on-chatbot.ts
+++ src/listeners/on-chatbot.ts
@@ -6,6 +6,7 @@ import {
 import { Client } from 'discord.js';
 import { GEMINI_TOKEN } from '../config';
 import { GeminiChat } from '../services/gemini.service';
+import { getRandomNumber } from '../utils/util-functions';
 
 export const openAiChat = async (client: Client) => {
   client.on('messageCreate', async (message) => {
@@ -14,7 +15,38 @@ export const openAiChat = async (client: Client) => {
 
     if (mention && mention.user.id === client.user?.id) {
       if (message.content.trim() === `<@${client.user.id}>`) {
-        message.reply('¡Hola! ¿En qué puedo ayudarte? B-Baka!');
+        const respuestas = [
+          'Q CHUCHA QUIERES MONGOL DE MRD',
+          'tu vieja',
+          'a!nuke',
+          'Ok color cartón',
+          'Q chucha pasa',
+          'o calla ps ramdon que ni conosco',
+          'aea',
+          'Aquí está tu vieja idi0ta mira como me la kacho Ooo tome señora tome',
+          'Tamare oe quien pcta le dio dr0ga a la llama csm miren cómo está hablando idi0ta de mrd',
+          'el que lo dice lo es',
+          'fue mi pene',
+          'calla perra',
+          'ag esta chola se enamoro de mi mirando mi foto que asco chola de mierda',
+          'mocosos de mrd y la ptamare',
+          'callate ctmre nadie te quiere escuchar oe paisano',
+          'oh wachi rctmr chibolo ctmr que chucha hablas',
+          'oh desahuevate rctmr ya anda duerme imbecil no todo es clase carajo',
+          'quien chucha eres tu webon',
+          'callate tu d0wn de mrd idi0ta solo peru es la mejor gastronomia preguntale a los a los extranjeros hijo de p3rr4',
+          'oe mrd corre bañate crj lee un libro y has algo por la vida',
+          'que xuxa vienes a hablar como pendejo rctmre ah vienes a awebar a la gente tu rctmre',
+          'o no jodan',
+          'chibolo mongol te meto un galletazo ctmre te desahuevo mongol de mierda dotero',
+          'oh mongol por que hablas asi? te apuesto a que si nos encontramos en la calle te hago correr oh chibolo baboso te quito todo lo que tienes oh recontra imbecil',
+          'que re mil pucta me mencionas a mi oe mongol de mierda',
+        ];
+
+        await message.reply(
+          respuestas[getRandomNumber(0, respuestas.length - 1)]
+        );
+        return;
       } else {
         try {
           const genAI = new GoogleGenerativeAI(GEMINI_TOKEN);
